866cd90876c011d276cc1bcb76b1099acd66c36c
Mj-Saini <manojsaini97285@gmail.com>
2024-05-14 22:12:05 +0530

    first committ

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+function toggleButton(activeButtonId, disabledButtonId) {
+  var activeButton = document.getElementById(activeButtonId);
+  var disabledButton = document.getElementById(disabledButtonId);
+
+  if (!activeButton.classList.contains("active")) {
+    activeButton.classList.add("active");
+    disabledButton.classList.remove("active");
+  }
+}
