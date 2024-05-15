e75bc26518b767b4be3ec39c81f388344104446c
Mj-Saini <manojsaini97285@gmail.com>
2024-05-15 11:16:44 +0530

    table

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,4 +7,16 @@ function toggleButton(activeButtonId, disabledButtonId) {
     disabledButton.classList.remove("active");
   }
 }
+  const radioButtons = document.querySelectorAll('input[type="radio"]');
 
+  // Add event listener to each radio button
+  radioButtons.forEach((radioButton) => {
+    radioButton.addEventListener("click", () => {
+      // Deselect other radio buttons in the same group
+      radioButtons.forEach((rb) => {
+        if (rb !== radioButton && rb.name === radioButton.name) {
+          rb.checked = false;
+        }
+      });
+    });
+  });
